dd99537cef5243aefad9881d8946ce1821a5b3bb
Dragoș Moldovan-Grünfeld <dragos.mold@gmail.com>
2026-01-18 21:31:58 +0000

    `to_md()` is renamed to `coverage_to_md()`

--- R/to_md.R
+++ R/to_md.R
@@ -1,7 +1,17 @@
-to_md <- function(x, align = "rr") {
+coverage_to_md <- function(x, align = "rr") {
   x |>
     digest_coverage() |>
     knitr::kable(
       align = align
     )
+}
+
+# x = head coverage
+# y = base coverage
+#
+# align = markdown column alignment
+diff_to_md <- function(x, y, pr_files, align = "rr") {
+  head_coverage <- digest_coverage(x)
+
+  base_coverage <- digest_coverage(y)
 }
