d449e3433945ed320626c271a43bfd8776847646
Dragoș Moldovan-Grünfeld <dragos.mold@gmail.com>
2026-01-18 12:50:06 +0000

    test_pr_details

--- tests/testthat/test-pr_details.R
+++ tests/testthat/test-pr_details.R
@@ -1,7 +1,15 @@
 test_that("get_pr_details() works", {
   owner <- "dragosmg"
   repo <- "covr2md"
-  pr_number <- 12
+  pr_number <- 14
 
   get_pr_details(owner, repo, pr_number)
+})
+
+test_that("get_changed_files() works", {
+  owner <- "dragosmg"
+  repo <- "covr2md"
+  pr_number <- 14
+
+  get_changed_files(owner, repo, pr_number)
 })
