af68e7715d8f187bd3ce6e90d88acaf8c183af2b
Dragoș Moldovan-Grünfeld <dragos.mold@gmail.com>
2026-01-18 12:42:56 +0000

    rename

--- tests/testthat/test-build_github_comment.R
+++ tests/testthat/test-build_github_comment.R
@@ -1,8 +1,8 @@
-test_that("build_comment() works", {
+test_that("compose_comment() works", {
   coverage <- readRDS(test_path("fixtures", "sample_coverage.RDS"))
 
   expect_snapshot(
-    build_comment(
+    compose_comment(
       coverage
     )
   )
