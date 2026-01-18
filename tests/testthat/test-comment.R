ce85b9998a8ba9cf0505c83d2fa85d92da052740
Dragoș Moldovan-Grünfeld <dragos.mold@gmail.com>
2026-01-18 21:32:14 +0000

    test for compose_comment()

--- tests/testthat/test-comment.R
+++ tests/testthat/test-comment.R
@@ -1,9 +1,14 @@
 test_that("compose_comment() works", {
-  coverage <- readRDS(test_path("fixtures", "sample_coverage.RDS"))
+  head_coverage <- readRDS(test_path("fixtures", "head_coverage.RDS"))
+  base_coverage <- readRDS(test_path("fixtures", "base_coverage.RDS"))
 
   expect_snapshot(
     compose_comment(
-      coverage
+      head_coverage,
+      base_coverage,
+      owner = "dragosmg",
+      repo = "covr2mddemo",
+      pr_number = 3
     )
   )
 })
