61ca78c7ebb346acb918ef22063da79d7ccd2a98
Dragoș Moldovan-Grünfeld <dragos.mold@gmail.com>
2026-01-19 21:15:22 +0000

    tests

--- tests/testthat/test-comment.R
+++ tests/testthat/test-comment.R
@@ -1,14 +1,44 @@
-test_that("compose_comment() works", {
-  head_coverage <- readRDS(test_path("fixtures", "head_coverage.RDS"))
-  base_coverage <- readRDS(test_path("fixtures", "base_coverage.RDS"))
-
-  expect_snapshot(
-    compose_comment(
-      head_coverage,
-      base_coverage,
+test_that("post_comment, get_comment_id & delete_comment work", {
+  skip_if_offline()
+  skip_on_ci()
+  expect_no_error(
+    a <- post_comment(
+      body = "this is a test :sweat_smile:",
       owner = "dragosmg",
       repo = "covr2mddemo",
-      pr_number = 3
+      pr_number = 3,
+      marker = "<!-- covr2md-test -->"
     )
   )
+
+  expect_s3_class(a, "gh_response")
+
+  expect_no_error(
+    get_comment_id(
+      owner = "dragosmg",
+      repo = "covr2mddemo",
+      pr_number = 3,
+      marker = "<!-- covr2md-test -->"
+    )
+  )
+
+  expect_identical(
+    get_comment_id(
+      owner = "dragosmg",
+      repo = "covr2mddemo",
+      pr_number = 3,
+      marker = "<!-- covr2md-code-coverage -->"
+    ),
+    3767770706
+  )
+
+  expect_no_error(
+    d <- delete_comment(
+      owner = "dragosmg",
+      repo = "covr2mddemo",
+      comment_id = a$id
+    )
+  )
+
+  expect_s3_class(d, "gh_response")
 })
