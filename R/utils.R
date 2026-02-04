fa8c797c684972450c10bccd4dc8e74b77720da3
Dragoș Moldovan-Grünfeld <dragos.mold@gmail.com>
2026-02-04 22:19:33 +0000

    interval helpers. fixes helpers for reporting missing lines Fixes #102

--- R/utils.R
+++ R/utils.R
@@ -1,3 +1,21 @@
 short_hash <- function(hash, n = 7) {
     stringr::str_sub(hash, 1, n)
 }
+
+find_intervals <- function(x) {
+    run_diff <- c(1, diff(x))
+    diff_list <- split(x, cumsum(run_diff != 1))
+
+    purrr::map_chr(diff_list, collapse_interval) |>
+        stringr::str_flatten_comma()
+}
+
+collapse_interval <- function(x) {
+    if (length(x) == 1) {
+        as.character(x)
+    } else if (length(x) == 2) {
+        stringr::str_flatten_comma(x)
+    } else {
+        paste0(x[1], "-", x[length(x)])
+    }
+}
--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -29,3 +29,49 @@ test_that("short_hash works", {
         "ca978112"
     )
 })
+
+test_that("collapse_interval", {
+    expect_identical(
+        collapse_interval(3),
+        "3"
+    )
+
+    expect_identical(
+        collapse_interval(c(3, 4)),
+        "3, 4"
+    )
+
+    expect_identical(
+        collapse_interval(1:8),
+        "1-8"
+    )
+
+    expect_identical(
+        collapse_interval(53:89),
+        "53-89"
+    )
+})
+
+test_that("find_intervals", {
+    expect_identical(
+        find_intervals(
+            c(55, 56, 57, 58, 60, 61, 62, 68, 69, 74, 77)
+        ),
+        "55-58, 60-62, 68, 69, 74, 77"
+    )
+
+    expect_identical(
+        find_intervals(c(3, 4, 5, 6, 9, 10, 15, 16, 17, 18, 25, 26, 27)),
+        "3-6, 9, 10, 15-18, 25-27"
+    )
+
+    expect_identical(
+        find_intervals(c(1, 2, 3, 4, 5, 6, 7, 8, 14, 15, 16, 17, 21)),
+        "1-8, 14-17, 21"
+    )
+
+    expect_identical(
+        find_intervals(c(53:57, 65:73, 89, 90, 91, 97)),
+        "53-57, 65-73, 89-91, 97"
+    )
+})
