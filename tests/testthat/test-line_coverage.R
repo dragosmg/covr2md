960feb0ef12a2b90ee71ee8494a73181297de401
Dragoș Moldovan-Grünfeld <dragos.mold@gmail.com>
2026-02-04 00:27:33 +0000

    tests

--- tests/testthat/test-line_coverage.R
+++ tests/testthat/test-line_coverage.R
@@ -14,7 +14,8 @@ test_that("compose_line_coverage_details with df", {
     line_cov <- tibble::tibble(
         file = c("R/foo.R", "R/bar.R", "R/baz.R"), # nolint
         lines_added = c(5, 4, 10),
-        lines_covered = c(2, 4, 6)
+        lines_covered = c(2, 4, 6),
+        which_lines = c("1-4, 6", "5-7, 9", "1-6, 10-13")
     )
 
     expect_s3_class(
