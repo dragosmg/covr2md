f7c2fd57fc13496782e1c095ae4aa7a5ef86407e
Dragoș Moldovan-Grünfeld <dragos.mold@gmail.com>
2026-02-06 00:27:57 +0000

    covr unexported function (#110)

--- R/covr2gh-package.R
+++ R/covr2gh-package.R
@@ -2,13 +2,6 @@
 #' @importFrom rlang .data
 "_PACKAGE"
 
-# Suppress R CMD check note
-# Namespace in Imports field not imported from: PKG
-#   All declared Imports should be used.
-# to_report_data uses htmltools
-ignore_unused_imports <- function() {
-    htmltools::a # nocov
-}
 
 ## usethis namespace: start
 ## usethis namespace: end
--- R/to_report_data.R
+++
@@ -1,4 +0,0 @@
-to_report_data <- utils::getFromNamespace(
-    "to_report_data",
-    "covr"
-)
